12dd19f9d91b03e386a5bf9e81abed53f69082f6
joaquinchavarria55 <joaquinchavarria55@gmail.com>
2026-06-16 15:12:21 -0600

    actualizacion db

--- src/config/db.js
+++ src/config/db.js
@@ -1,24 +1,23 @@
 const { Pool } = require('pg');
 
+
+
 const pool = new Pool({
-<<<<<<< HEAD
   host: 'localhost',
   port: 5432,
   user: 'postgres',
   password: 'admin123',
   database: 'taller_mecanico',
-=======
-  // Si existe la variable de entorno (Docker), la usa. Si no, usa 'localhost'.
+
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
   database: process.env.DB_NAME || 'taller_mecanico',
->>>>>>> 26de220fb97122033ab3b87f93bcaa5b7e92a79e
 });
 
 pool.connect()
-  .then(() => console.log('Conectado a PostgreSQL exitosamente'))
+  .then(() => console.log('Conectado a PostgreSQL'))
   .catch(err => console.error('Error al conectar a PostgreSQL:', err));
 
 module.exports = pool;
