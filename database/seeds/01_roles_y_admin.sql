8d0ee1356dc675f45474336b27149fd3320d5a5d
joaquinchavarria55 <joaquinchavarria55@gmail.com>
2026-06-19 15:26:32 -0600

    fix: actualizar hash de admin en seed

--- database/seeds/01_roles_y_admin.sql
+++ database/seeds/01_roles_y_admin.sql
@@ -9,7 +9,7 @@ VALUES (
   'Administrador Principal',
   'admin',
   'admin@sigta.com',
-  '$2a$10$7wXoA/X3ZzPCHpWnU.8HyeS0m9HqXQe695v31wVlRjC5rWJjM9vbe',
+  '$2b$10$aCvu87De0wTOOclfLRcPmOrrELOKsDWGrRTK3PSqPWTDN5K/t8I3a',
   (SELECT id FROM roles WHERE nombre = 'administrador')
 ) 
 ON CONFLICT (nombre_usuario) DO NOTHING;
