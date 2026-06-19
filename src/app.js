a3bc93ba19566ab88f76789f3863fc0a756c7ce5
joaquinchavarria55 <joaquinchavarria55@gmail.com>
2026-06-19 15:12:20 -0600

    feat: CRUD de clientes actualizado

--- src/app.js
+++ src/app.js
@@ -2,24 +2,26 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-
 const authRoutes = require('./routes/authRoutes');
 const usuarioRoutes = require('./routes/usuarioRoutes');
-
-const app = express();
 const clienteRoutes = require('./routes/clienteRoutes');
 
-app.use('/api/clientes', clienteRoutes);
+const app = express();
+
 
 app.use(cors());
 app.use(express.json());
 
+
 app.use('/api/auth', authRoutes);
 app.use('/api/usuarios', usuarioRoutes);
+app.use('/api/clientes', clienteRoutes);
+
 
 app.get('/api/health', (req, res) => {
   res.json({ status: 'OK', message: 'Servidor funcionando' });
 });
+
 app.use((req, res) => {
   res.status(404).json({
     success: false,
@@ -31,7 +33,7 @@ app.use((err, req, res, next) => {
   console.error('Error:', err);
   res.status(500).json({
     success: false,
-    message: 'Error servidor'
+    message: 'Error interno del servidor'
   });
 });
 
