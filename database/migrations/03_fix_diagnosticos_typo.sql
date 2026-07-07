c1d3e6446b52019c7a4132cfec8b945f8f6bffe7
rigo <rguillen.jose@gmail.com>
2026-07-07 11:30:24 -0600

    Arreglando ordenes

--- database/migrations/03_fix_diagnosticos_typo.sql
+++
@@ -1,9 +0,0 @@
-DO $$
-BEGIN
-    IF EXISTS (
-        SELECT 1 FROM information_schema.columns
-        WHERE table_name = 'diagnosticos' AND column_name = 'decripcion_falla'
-    ) THEN
-        ALTER TABLE diagnosticos RENAME COLUMN decripcion_falla TO descripcion_falla;
-    END IF;
-END $$;
