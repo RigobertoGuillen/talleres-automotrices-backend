b8329a81ac8a1246db617b87da453f1c10ad047d
rigo <rguillen.jose@gmail.com>
2026-07-23 15:04:09 -0600

    agregar rutas dashboard2

--- src/constants/queries/dashboardQueries.js
+++ src/constants/queries/dashboardQueries.js
@@ -4,6 +4,6 @@ module.exports = {
       (SELECT COUNT(*) FROM ordenes_trabajo WHERE estado IN ('recibido', 'en reparacion')) AS ordenes_progreso,
       (SELECT COUNT(*) FROM ordenes_trabajo WHERE estado = 'listo') AS vehiculos_listos,
       (SELECT COUNT(*) FROM diagnosticos WHERE estado = 'pendiente') AS diagnosticos_pendientes,
-      (SELECT COUNT(*) FROM inventario WHERE stock_actual <= stock_minimo) AS alertas_inventario
+      (SELECT COUNT(*) FROM stock_repuestos WHERE cantidad_disponible <= cantidad_minima) AS alertas_inventario
   `
 };
