3fe903bb11b9c4bebde61207064b88b2f8727b1f
rigo <rguillen.jose@gmail.com>
2026-07-26 11:46:25 -0600

    feat: dashboard cambios2

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -1,3 +1,24 @@
+javascript
+const pool = require('../config/db');
+const dashboardQueries = require('../constants/queries/dashboardQueries');
+
+const getStats = async (req, res, next) => {
+  try {
+    const result = await pool.query(dashboardQueries.GET_STATS);
+    const row = result.rows[0] || {};
+
+    res.json({
+      ordenesProgreso: parseInt(row.ordenes_progreso || 0, 10),
+      vehiculosListos: parseInt(row.vehiculos_listos || 0, 10),
+      diagnosticosPendientes: parseInt(row.diagnosticos_pendientes || 0, 10),
+      alertasInventario: parseInt(row.alertas_inventario || 0, 10)
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+
 const getOrdenesRecientes = async (req, res) => {
   try {
     const result = await pool.query(dashboardQueries.ORDENES_RECIENTES);
